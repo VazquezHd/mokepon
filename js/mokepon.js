e7216dc36ef259c504ed3675b668388afb8fbfb9
mictlanhalley <vazquez.dg22@gmail.com>
2022-09-16 17:13:50 -0500

    cambios 15 sept

--- js/mokepon.js
+++ js/mokepon.js
@@ -19,6 +19,9 @@ const ataquesDelEnemigo = document.getElementById('ataques-del-enemigo')
 const contenedorTarjetas = document.getElementById('contenedorTarjetas')
 const contenedorAtaques = document.getElementById('contenedorAtaques') 
 
+const sectionVerMapa = document.getElementById('ver-mapa')
+const mapa = document.getElementById('mapa')
+
 let mokepones = [] //corchetes cuadrados es un arreglo
 let ataqueJugador = [] 
 let ataqueEnemigo = [] 
@@ -39,6 +42,7 @@ let victoriasJugador = 0
 let victoriasEnemigo = 0
 let vidasJugador = 3  
 let vidasEnemigo = 3
+let lienzo = mapa.getContext("2d")
 
 //esto es una clase y dentro esta un objeto con diferentes priopiedades
 class Mokepon {
@@ -47,6 +51,12 @@ class Mokepon {
         this.foto = foto
         this.vida = vida
         this.ataques = []
+        this.x = 20
+        this.y = 30
+        this.ancho = 80
+        this.alto = 80
+        this.mapaFoto = new Image ()
+        this.mapaFoto.src = foto 
     }
 }
 //objetos de esa clase
@@ -83,6 +93,7 @@ function iniciarJuego(){
     
     sectionReiniciar
     .style.display = 'none'
+    sectionVerMapa.style.display = 'none'
 
     mokepones.forEach((mokepon)=>{
         opcionDeMokepones = `
@@ -107,7 +118,14 @@ function iniciarJuego(){
 }
 function seleccionarMascotaJugador(){   
     sectionSeleccionarMascota.style.display = 'none'
-    sectionSeleccionarAtaque.style.display = 'flex'
+    //sectionSeleccionarAtaque.style.display = 'flex'
+
+    //esto es canvas
+    sectionVerMapa.style.display = 'flex'
+
+
+
+
         //else if son condicionales, como tenemos varias secciones, colocamos "si no es esta opcion es eCapipeposta otra" y de ese modo podemos ir de una a otra seleccion
     if (inputHipodoge.checked) {
         spanMascotaJugador.innerHTML= inputHipodoge.id
@@ -328,6 +346,22 @@ function reiniciarJuego (){
 // esta es la formula para la aleatoriedad
 function aleatorio(min,max){
     return Math.floor(Math.random()*(max - min + 1)+ min)
+}
+
+function pintarPersonaje() {
+    lienzo.clearRect(0, 0, mapa.width, mapa.height)
+    lienzo.drawImage(
+        capipepo.mapaFoto,
+        capipepo.x,
+        capipepo.y,
+        capipepo.ancho,
+        capipepo.alto,
+    )
+}
+
+function moverCapipepo() {
+    capipepo.x = capipepo.x + 5
+    pintarPersonaje()
 }
 //desde aqui le pedimos que escuche cuando cargue la pagina para que inicie el juego
 window.addEventListener('load', iniciarJuego)
